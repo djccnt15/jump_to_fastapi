827fd012cc5a204cff3cc7392d35f7ba3bb2ed94
Jay.D <djccnt15@gmail.com>
2025-07-20 11:44:00 +0900

    initiate fastapi project

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from jump-to-fastapi!")
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+origins = [
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
