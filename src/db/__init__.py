d7b0f89ba4828dbe0e6778780855cc369f98c1ad
Jay.D <djccnt15@gmail.com>
2025-07-29 21:18:16 +0900

    update entity to SQLAlchemy 2.0 style

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,3 +1,4 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
-Base = declarative_base()
+
+class BaseEntity(DeclarativeBase): ...
--- src/db/entity.py
+++ src/db/entity.py
@@ -1,33 +1,35 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text
-from sqlalchemy.orm import relationship
+from datetime import datetime
 
-from src.db import Base
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.db import BaseEntity
 
 
-class QuestionEntity(Base):
+class QuestionEntity(BaseEntity):
     __tablename__ = "question"
 
-    id = Column(Integer, primary_key=True)
-    subject = Column(String, nullable=False)
-    content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    subject: Mapped[str] = mapped_column(String, nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    create_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
 
 
-class AnswerEntity(Base):
+class AnswerEntity(BaseEntity):
     __tablename__ = "answer"
 
-    id = Column(Integer, primary_key=True)
-    content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
-    question_id = Column(Integer, ForeignKey("question.id"))
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    create_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    question_id: Mapped[int] = mapped_column(Integer, ForeignKey("question.id"))
 
-    question = relationship("QuestionEntity", backref="answers")
+    question: Mapped[QuestionEntity] = relationship("QuestionEntity", backref="answers")
 
 
-class UserEntity(Base):
+class UserEntity(BaseEntity):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
