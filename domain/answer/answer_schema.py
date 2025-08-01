48614a49cfe385036b197785d4043f0e646dc17e
Jay.D <djccnt15@gmail.com>
2025-08-01 09:55:16 +0900

    create Question/Answer vote function

--- domain/answer/answer_schema.py
+++ domain/answer/answer_schema.py
@@ -21,6 +21,7 @@ class Answer(BaseModel):
     question_id: int
     modify_date: datetime.datetime | None = None
     user: User | None
+    voter: list[User] = []
 
 
 class AnswerUpdate(AnswerCreate):
@@ -28,4 +29,8 @@ class AnswerUpdate(AnswerCreate):
 
 
 class AnswerDelete(BaseModel):
+    answer_id: int
+
+
+class AnswerVote(BaseModel):
     answer_id: int
--- domain/question/question_schema.py
+++ domain/question/question_schema.py
@@ -13,6 +13,7 @@ class Question(BaseModel):
     modify_date: datetime.datetime | None = None
     answers: list[Answer] = []
     user: User | None
+    voter: list[User] = []
 
 
 class QuestionCreate(BaseModel):
@@ -36,4 +37,8 @@ class QuestionUpdate(QuestionCreate):
 
 
 class QuestionDelete(BaseModel):
+    question_id: int
+
+
+class QuestionVote(BaseModel):
     question_id: int
