6c5523389f0240a20a9d252db26e887bb5bcede1
Jay.D <djccnt15@gmail.com>
2025-09-29 21:25:04 +0900

    create get_question_list_v2 in question_crud

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from domain.question.question_schema import QuestionCreate, QuestionUpdate
 from sqlalchemy import and_
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, selectinload
 
 from models import Answer, Question, User
 
@@ -30,6 +30,58 @@ def get_question_list(db: Session, skip: int = 0, limit: int = 10, keyword: str 
     total = question_list.distinct().count()
     question_list = (
         question_list.order_by(Question.create_date.desc())
+        .offset(skip)
+        .limit(limit)
+        .distinct()
+        .all()
+    )
+    return total, question_list  # (전체 건수, 페이징 적용된 질문 목록)
+
+
+def get_question_list_v2(
+    db: Session,
+    *,
+    skip: int = 0,
+    limit: int = 10,
+    keyword: str = "",
+) -> tuple[int, list[Question]]:
+    # TODO. (refactoring) 함수 기능 분리
+    question_list = db.query(Question)
+    if keyword:
+        search = "%%{}%%".format(keyword)
+        sub_query = (
+            db.query(
+                Answer.question_id,
+                Answer.content,
+                User.username,
+            )
+            .outerjoin(User, Answer.user_id == User.id)
+            .subquery()
+        )
+        question_list = (
+            question_list.outerjoin(User)
+            .outerjoin(sub_query, sub_query.c.question_id == Question.id)
+            .filter(
+                Question.subject.ilike(search)  # 질문제목
+                | Question.content.ilike(search)  # 질문내용
+                | User.username.ilike(search)  # 질문작성자
+                | sub_query.c.content.ilike(search)  # 답변내용
+                | sub_query.c.username.ilike(search)  # 답변작성자
+            )
+        )
+    total = question_list.distinct().count()
+    question_list = (
+        question_list.options(
+            # question author
+            selectinload(Question.user),
+            # question voters
+            selectinload(Question.voter),
+            # answer author
+            selectinload(Question.answers).selectinload(Answer.user),
+            # answer voters
+            selectinload(Question.answers).selectinload(Answer.voter),
+        )
+        .order_by(Question.create_date.desc())
         .offset(skip)
         .limit(limit)
         .distinct()
